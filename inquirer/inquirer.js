d3ad426313a79bba25188e683132131c55817ec3
Buff4eyes <claudemokbel@gmail.com>
2021-12-16 00:15:09 -0800

    added initial inquirer question

--- inquirer/inquirer.js
+++ inquirer/inquirer.js
@@ -1,1 +1,20 @@
 var inquirer = require('inquirer');
+
+inquirer.prompt(
+  [{
+    type: 'list',
+    name: 'todo',
+    message:'What would you like to do?'
+    choices:['Add Department(s)','Add Role(s)','Add Employee(s)', 'View Departments', 'View Roles', 'View Employees','Update Employee Roles', 'Update Employee Manager(s)', 'View Employees By Manager', 'Delete Department(s)', 'Delete Role(s)', 'Delete Employee(s)', 'View Total Budget']
+  }]
+)
+.then((answers) => {
+
+})
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log('Prompt could not be rendered')
+    } else {
+      console.log(error)
+    }
+  });
