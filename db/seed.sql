af9acb904c51a36a763302e84c77fda182f72616
Buff4eyes <claudemokbel@gmail.com>
2021-12-15 23:26:53 -0800

    database tables made/1 router made

---
+++ db/seed.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department {
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL
+};
+
+CREATE TABLE role {
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  FOREIGN KEY departmentid REFERENCES deparment(id)
+};
+
+CREATE TABLE employee {
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  FOREIGN KEY role_id REFERENCES role(id) NOT NULL,
+  FOREIGN KEY manager_id REFERENCES 
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,32 @@
 const router = require('express').Router()
 const db = require('../db')
 
+router.get('/department', (req, res) => {
+  db.query('SELECT * FROM department', (err, department) => {
+    if (err) { console.log(users) }
+    res.json(users)
+  })
+})
+
+router.post('/department', (req, res) => {
+  db.query('INSERT INTO department SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.put('/department/:id', (req, res) => {
+  db.query('UPDATE department SET ? WHERE ?', [req.body, { id: req.params.id }], err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.delete('/users/:id', (req, res) => {
+  db.query('DELETE FROM department WHERE ?', { id: req.params.id }, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
 
 module.exports = router
