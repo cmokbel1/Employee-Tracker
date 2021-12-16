d35bfa73c7ff8ee5109fd8b0c6344a7b26d4bc69
Buff4eyes <claudemokbel@gmail.com>
2021-12-15 23:38:21 -0800

    added employee and role routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const db = require('../db')
 
+// DEPARTMENT ROUTES
+
 router.get('/department', (req, res) => {
   db.query('SELECT * FROM department', (err, department) => {
-    if (err) { console.log(users) }
+    if (err) { console.log(department) }
     res.json(users)
   })
 })
@@ -29,4 +31,62 @@ router.delete('/users/:id', (req, res) => {
   })
 })
 
+// ROLE ROUTES
+
+router.get('/role', (req, res) => {
+  db.query('SELECT * FROM role', (err, role) => {
+    if (err) { console.log(role) }
+    res.json(users)
+  })
+})
+
+router.post('/role', (req, res) => {
+  db.query('INSERT INTO role SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.put('/role/:id', (req, res) => {
+  db.query('UPDATE role SET ? WHERE ?', [req.body, { id: req.params.id }], err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.delete('/role/:id', (req, res) => {
+  db.query('DELETE FROM role WHERE ?', { id: req.params.id }, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+// EMPLOYEE ROUTES
+router.get('/employee', (req, res) => {
+  db.query('SELECT * FROM employee', (err, employee) => {
+    if (err) { console.log(employee) }
+    res.json(users)
+  })
+})
+
+router.post('/employee', (req, res) => {
+  db.query('INSERT INTO employee SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.put('/employee/:id', (req, res) => {
+  db.query('UPDATE employee SET ? WHERE ?', [req.body, { id: req.params.id }], err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+router.delete('/employee/:id', (req, res) => {
+  db.query('DELETE FROM employee WHERE ?', { id: req.params.id }, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
 module.exports = router
